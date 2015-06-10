6712d664412edc5ab2d1d511eb541f0ac7972b2a
anders <anders@Bk-Studie>
2015-06-10 10:11:46 +0200

    DriverEngine added

---
+++ PCFiles/src/engine/DriverEngine.java
@@ -0,0 +1,17 @@
+package engine;
+
+public class DriverEngine {
+	/* TODO:
+	 * The DriverEngine calculates the coordinates of the balls and robot.
+	 * 
+	 * It gives instructions to the robot through connection.PCConn
+	 * 
+	 * it should make a decision which ball it wish to go for and figure out which
+	 * string of instructions has to be sent to fulfill the decision. 
+	 * 
+	 * it waits for a response from the robot, every time (?) it sends an instruction
+	 * 
+	 * A flowchart should be made for this class for the common understanding of its process
+	 * 
+	 */
+}
