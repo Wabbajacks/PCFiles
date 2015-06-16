ba17cdc6b44ea66e8e8cae33558987b31d9e5ca0
Jesper <jesperbn90@gmail.com>
2015-06-16 10:38:57 +0200

    p2d[]

--- PCFiles/src/imgProcessing/ImgInfo.java
+++ PCFiles/src/imgProcessing/ImgInfo.java
@@ -4,21 +4,22 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 public class ImgInfo {
-	private ArrayList<Point2D> balls;
+	private Point2D[] balls;
 	private Point2D[] obstacle;
 	private Point2D[] frame;
 	private Point2D[] robot;
 	private Point2D[] goals;
 	
 	public ImgInfo(ArrayList<Point2D> balls, Point2D[] obstacle, Point2D[] frame, Point2D[] robot, Point2D[] goals) {
-		this.balls = balls;
 		this.obstacle = obstacle;
 		this.frame = frame;
 		this.robot = robot;
 		this.goals = goals;
+		this.balls = new Point2D[balls.size()];
+		balls.toArray(this.balls);
 	}
 
-	public ArrayList<Point2D> getBalls() {
+	public Point2D[] getBalls() {
 		return balls;
 	}
 
