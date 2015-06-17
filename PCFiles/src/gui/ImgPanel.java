293f17f2efd21c9260a6d37a61478d48fbcb0754
Kristin <Kristin@10.16.128.208>
2015-06-17 17:44:18 +0200

    l

--- PCFiles/src/gui/ImgPanel.java
+++ PCFiles/src/gui/ImgPanel.java
@@ -1,29 +1,40 @@
 package gui;
 
+import gui.utils.CreateTextimage;
+
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import org.opencv.core.Mat;
 
 import net.miginfocom.swing.MigLayout;
 
 public class ImgPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
-	private BufferedImage img;
+	private BufferedImage bimg;
+	private JLabel image;
 	private JLabel title;
 	
 	public ImgPanel() {
-		setPreferredSize(new Dimension(580, 200));
+		setPreferredSize(new Dimension(587, 240));
 		setLayout(new MigLayout());
 		
-		img = new Mat();
+		try {
+			bimg = ImageIO.read(new File("images/scrCap.jpg"));
+		} catch(IOException e) {
+			bimg = CreateTextimage.createImage("No screen capture available.");
+		}
+		
+		image = new JLabel(new ImageIcon(bimg));
 		title = new JLabel("Latest image");
 		
 		add(title, "wrap");
+		add(image);
 	}
 }
