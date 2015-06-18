e4f4b7d422f86ea16c3b272a9738cc8881728431
anders <anders@Bk-Studie>
2015-06-18 23:25:02 +0200

    AlgoEngine for the loverBoyz Maltez <3

--- PCFiles/src/engine/AlgoEngine.java
+++ PCFiles/src/engine/AlgoEngine.java
@@ -1,11 +1,12 @@
 package engine;
 
-import java.awt.geom.Line2D;
+//import java.awt.geom.Line2D;
 //import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 
+import math.geom2d.line.*;
 import math.geom2d.Point2D;
 import math.geom2d.Vector2D;
 
@@ -65,21 +66,59 @@ public class AlgoEngine {
 		commands.clear();
 	}
 	
-	public void run(Point2D[] balls, Point2D[] robot, Point2D[] wall, Point2D[] obst){
+	public void run(Point2D[] balls, Point2D[] robot, Point2D[] wall, Point2D[] obst, Point2D[] goals){
 		// Obstacle can be made into a rectangle if needed
 		commands.clear();
 		switch(state){
+		/* START */
 		case "START":
 			state = "CALCRUTE";
 		case "CALCRUTE":
 			setNearestBall(balls, robot, wall, obst);
-			robotV = new Vector2D(robot[1], robot[0]);
-			break;
+			
+			state = "FETCHING";
 		case "FETCHING":
+			/* FETCHING
+			 * yes
+			 * */
+			robotV = new Vector2D(robot[1], robot[0]);
+			courseV = new Vector2D(robot[0], targetBall);
+			if(checkInterSection(obst, robot[0], targetBall)){
+				state = "REDIRECTED";
+				
+				if (Math.abs(robot[0].getX() - targetBall.getX()) > Math.abs(robot[0].getY() - targetBall.getY())){
+					int i = 50;
+					if (obst[0].getY() < robot[0].getY()) i = -50;
+					courseV = new Vector2D(robot[0], new Point2D(robot[0].getX(), obst[0].getY()+i));
+				} else {
+					int i = 50;
+					if (obst[0].getX() < robot[0].getX()) i = -50;
+					courseV = new Vector2D(robot[0], new Point2D(obst[0].getX()+i, robot[0].getY()));
+				}
+				break;
+			}
+			int degree = degree(robotV, courseV);
+			
+			break;
+		case "REDIRECTED":
+			/* REDIRECTED
+			 * When the Robot is trying to move trough the obstacle in the middle it should be put in this state,
+			 * moving at a redirected course, moving around the obstacle in the middle,
+			 * so when it reaches its goal it should not increment the BallCatchCounter*/
+			
+			
 			break;
 		case "DELIVER":
+			/* DELIVER should have 2 states, and should be in this state when the BallCatchCounter is at its limit:
+			 * State 1: driver over to the front of the goal coordinate (like +-20 to x-axis)
+			 * State 2: only change the direction so the vectors is parallel (where courseV is directly to the goal,
+			 * 	and then VOMIT ALL OVER THAT GOAL!! */
+			//Left goal is little goal
 			break;
 		case "FINNISHED":
+			/* FINNISHED
+			 * When there is no more balls let on the fields it should go to this state.
+			 * */
 			break;
 		default:
 			System.out.println("What y'all doing here !?");
@@ -136,8 +175,11 @@ public class AlgoEngine {
 		return degree;
 	}
 	
-	private boolean checkInterSection(Point2D[] obst, Line2D l) {
+	private boolean checkInterSection(Point2D[] obst, Point2D startL, Point2D endL) {
 		Rectangle2D r = new Rectangle2D.Double(obst[0].getX(), obst[0].getY(), (obst[1].getX()-obst[0].getX()), (obst[0].getY()-obst[1].getY()));
+		java.awt.geom.Line2D l = new java.awt.geom.Line2D.Double(
+				new java.awt.geom.Point2D.Double(startL.getX(), startL.getY()), 
+				new java.awt.geom.Point2D.Double(endL.getX(), endL.getY()));
 		
 		return l.intersects(r);
 	}
